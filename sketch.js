38e70c3fe8e1087a9e5f55e8ff54e2d6ce39be5e
HarshVajani <70755020+HarshVajani@users.noreply.github.com>
2020-12-13 16:44:05 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,101 +1,110 @@
-const Engine = Matter.Engine;
-const World = Matter.World;
-const Bodies = Matter.Bodies;
-const Body = Matter.Body;
+var dog,sadDog,happyDog,garden,washroom, database;
+var foodS,foodStock;
+var fedTime,lastFed,currentTime;
+var feed,addFood;
+var foodObj;
+var gameState,readState;
 
-var engine, world;
-var ground;
-var stand1, stand2, stand3, stand4, stand5, stand6, stand7;
-var particles = [];
-var plinkos = [];
-
+function preload(){
+sadDog=loadImage("Images/Dog.png");
+happyDog=loadImage("Images/happy dog.png");
+garden=loadImage("Images/Garden.png");
+washroom=loadImage("Images/Wash Room.png");
+bedroom=loadImage("Images/Bed Room.png");
+}
 
 function setup() {
-  createCanvas(480,800);
-
-  engine = Engine.create();
-  world = engine.world;
-
-  ground = new Ground(0, 780, 1200, 10);
-
-  stand1 = new Division(0, 680, 5, 200);
-  stand2 = new Division(80, 680, 10, 200);
-  stand3 = new Division(170, 680, 10, 200);
-  stand4 = new Division(250, 680, 10, 200);
-  stand5 = new Division(330, 680, 10, 200);
-  stand6 = new Division(410, 680, 10, 200);
-  stand7 = new Division(475, 680, 5, 200);
-
-  for (var j = 75; j <=width; j=j+50) 
-  {
+  database=firebase.database();
+  createCanvas(400,500);
   
-     plinkos.push(new Plinko(j,75,10));
-  }
+  foodObj = new Food();
 
-  for (var j = 50; j <=width-10; j=j+50) 
-  {
-  
-     plinkos.push(new Plinko(j,175,10));
-  }
+  foodStock=database.ref('Food');
+  foodStock.on("value",readStock);
 
-   for (var j = 75; j <=width; j=j+50) 
-  {
-  
-     plinkos.push(new Plinko(j,275,10));
-  }
+  fedTime=database.ref('FeedTime');
+  fedTime.on("value",function(data){
+    lastFed=data.val();
+  });
 
-   for (var j = 50; j <=width-10; j=j+50) 
-  {
+  //read game state from database
+  readState=database.ref('gameState');
+  readState.on("value",function(data){
+    gameState=data.val();
+  });
+   
+  dog=createSprite(200,400,150,150);
+  dog.addImage(sadDog);
+  dog.scale=0.15;
   
-     plinkos.push(new Plinko(j,375,10));
-     
-  }
+  feed=createButton("Feed the dog");
+  feed.position(700,95);
+  feed.mousePressed(feedDog);
 
-  
-
-  
-  
+  addFood=createButton("Add Food");
+  addFood.position(800,95);
+  addFood.mousePressed(addFoods);
 }
 
 function draw() {
-   background(0);
-
-   Engine.update(engine);
-
-   ground.display();
-
-   stand1.display();
-   stand2.display();
-   stand3.display();
-   stand4.display();
-   stand5.display();
-   stand6.display();
-   stand7.display();
-
-   for (var i = 0; i < plinkos.length; i++) {
-     
-    plinkos[i].display();
-    
-  }
-  
-  if(frameCount%60===0){
-   particles.push(new Particle(random(width/2-30, width/2+30), 10,10));
+  currentTime=hour();
+  if(currentTime==(lastFed+1)){
+      update("Playing");
+      foodObj.garden();
+   }else if(currentTime==(lastFed+2)){
+    update("Sleeping");
+      foodObj.bedroom();
+   }else if(currentTime>(lastFed+2) && currentTime<=(lastFed+4)){
+    update("Bathing");
+      foodObj.washroom();
+   }else{
+    update("Hungry")
+    foodObj.display();
+   }
    
- }
-
- for (var j = 0; j < particles.length; j++) {
-  
-    particles[j].display();
-  }
-
-   
-
+   if(gameState!="Hungry"){
+     feed.hide();
+     addFood.hide();
+     dog.remove();
+   }else{
+    feed.show();
+    addFood.show();
+    dog.addImage(sadDog);
+   }
+ 
+  drawSprites();
 }
-function mousePressed() 
-{
-    if(gameState1=="end"){
-        count++;
-        particle=new Particle(mouseX,10,10,10);
-    }
 
+//function to read food Stock
+function readStock(data){
+  foodS=data.val();
+  foodObj.updateFoodStock(foodS);
+}
+
+
+//function to update food stock and last fed time
+function feedDog(){
+  dog.addImage(happyDog);
+
+  foodObj.updateFoodStock(foodObj.getFoodStock()-1);
+  database.ref('/').update({
+    Food:foodObj.getFoodStock(),
+    FeedTime:hour(),
+    gameState:"Hungry"
+  })
+}
+
+//function to add food in stock
+function addFoods(){
+  foodS++;
+  database.ref('/').update({
+    Food:foodS
+  })
+}
+
+//update gameState
+function update(state){
+  database.ref('/').update({
+    gameState:state
+  })
 }
